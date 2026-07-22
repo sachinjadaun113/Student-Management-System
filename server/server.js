e47c0a1f0c554125a1c3b1b7354efb35ab0d7567
sachin kumar <sachinjadaun@gmail.com>
2026-07-22 20:40:09 +0530

    Configure Express server with dotenv, CORS, and nodemon

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
+require("dotenv").config();
+
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
-const port = 5000;
+app.use(cors());
+app.use(express.json());
 
-app.get("/",(req,res) => {
-    res.send("Student Management System Backend Running 🚀");
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 Student Management System Backend is Running");
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
