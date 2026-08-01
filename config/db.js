d88b1f62b4efbced3f3504b59b93e0675b60b709
sachin kumar <sachinjadaun@gmail.com>
2026-08-01 14:37:06 +0530

    connect with mongoDB atlas

--- config/db.js
+++ config/db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected Successfully");
+    // console.log("MongoDB Connected Successfully");
+    console.log("MongoDB Atlas Connected Successfully");
   } catch (error) {
     console.error("Database Connection Failed:", error.message);
     process.exit(1);
